fbc9d30ff31ba44126b62e657fc44d33623c3c49
kaimelis <kaimelisvy24@outlook.com>
2019-07-30 17:16:48 +0200

    Test objects added and a way to populate a list with prefabs in a project.

---
+++ VariousTools/Assets/Tools/PrefabSpawner/Scripts/Editor/PrefabSpawner.cs
@@ -0,0 +1,33 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using UnityEngine.UI;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using System.Collections.Generic;
+public class PrefabSpawner : OdinEditorWindow
+{
+    [MenuItem("KoEditor/Tools/PrefabSpawner", priority = 0)]
+    public static void ShowWindow()
+    {
+        GetWindow<PrefabSpawner>().Show();
+    }
+
+    public static PrefabSpawner Instance()
+    {
+        return GetWindow<PrefabSpawner>();
+    }
+
+   [AssetList(CustomFilterMethod = "IsPrefab", AssetNamePrefix = "prefab")]
+   [InlineEditor(InlineEditorModes.SmallPreview)]
+    public List<Transform> Prefabs;
+
+    private bool IsPrefab(Transform obj)
+    {
+        return PrefabUtility.GetPrefabAssetType(obj) == PrefabAssetType.Regular;
+    }
+
+}
+
+#endif
