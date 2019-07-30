68418b4d68c111a030434c4b3fdca0eff473a40f
kaimelis <kaimelisvy24@outlook.com>
2019-07-30 12:04:36 +0200

    Added free camera roam in editor script

---
+++ VariousTools/Assets/FreeCameraTool/Scripts/Editor/FreeCamera_Editor.cs
@@ -0,0 +1,53 @@
+﻿#if UNITY_EDITOR
+
+using UnityEngine;
+using UnityEditor;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+
+public class FreeCamera_Editor : OdinEditorWindow
+{
+    [MenuItem("Custom/Editor", priority = 0)]
+    public static void ShowWindow()
+    {
+        GetWindow<FreeCamera_Editor>().Show();
+    }
+    
+    public static FreeCamera_Editor Instance()
+    {
+        return GetWindow<FreeCamera_Editor>();
+    }
+
+    [HideInEditorMode]
+    [InfoBox("Warning: Application is playing.", InfoMessageType.Warning)]
+    [ReadOnly]
+    private readonly bool warningAppIsPlaying;
+
+
+    #region SCENE
+    [TabGroup("MAIN", "SCENE")]
+    [InfoBox("Updates the GameView camera to match SceneView camera Only works if all CineMachine cameras are disabled.")]
+    public bool syncEditorCam = false;
+    #endregion
+
+
+    [TabGroup("MAIN", "TAB")]
+    [InfoBox("Updates the GameView camera to match SceneView camera Only works if all CineMachine cameras are disabled.")]
+    public bool another = false;
+
+    private void Update()
+    {
+        if (syncEditorCam)
+        {
+            Camera mainCamera = Camera.main;
+            Camera currentCamera = Camera.current;
+            if (mainCamera != null && currentCamera != null)
+            {
+                mainCamera.transform.position = currentCamera.transform.position;
+                mainCamera.transform.rotation = currentCamera.transform.rotation;
+            }
+        }
+    }
+}
+
+#endif
