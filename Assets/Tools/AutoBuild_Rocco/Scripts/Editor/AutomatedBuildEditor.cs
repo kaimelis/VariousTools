e7cd63fc2f244af8426f85363cd0f0f4acb19466
Kotryna <kaimelisvy24@outlook.com>
2019-10-07 11:51:20 +0200

    wokring version of new build tool

--- Assets/Tools/AutoBuild_Rocco/Scripts/Editor/AutomatedBuildEditor.cs
+++ Assets/Tools/AutoBuild_Rocco/Scripts/Editor/AutomatedBuildEditor.cs
@@ -1,8 +1,6 @@
 ﻿#if UNITY_EDITOR
 using Sirenix.OdinInspector;
 using Sirenix.OdinInspector.Editor;
-using Sirenix.Serialization;
-using Sirenix.Utilities.Editor;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -28,167 +26,237 @@ namespace Custom.Tool
         public string username;
     }
 
+    public struct SceneInfo
+    {
+       public bool BuildRight;
+       public bool BuildLeft;
+       public bool BuildCenter;
+    }
+
     public class AutomatedBuildEditor : OdinEditorWindow
     {
-        [SerializeField]
-        public AutomatedBuildSettings buildSettings;
+        public SceneInfo SceneBuildType;
 
-        private Dictionary<SceneAsset, bool> _settingsDictionary = new Dictionary<SceneAsset, bool>();
-        private int _buildNumber = 0;
-        private int _finishedBuilds = 0;
-        private string _currentName;
+        [ReadOnly]
+        public SceneAsset rightScene;
+        [ReadOnly]
+        public SceneAsset leftScene;
+        [ReadOnly]
+        public SceneAsset centerScene;
+        [ReadOnly]
+        public List<SceneAsset> sceneList = new List<SceneAsset>();
+        private List<EditorBuildSettingsScene> editorBuilds = new List<EditorBuildSettingsScene>();
         private string _buildPath;
         private BuildType _buildType;
-
+        private int _buildCount = 0;
+        private int _currentBuild = 0;
+      
         [MenuItem("CustomTools/AutoBuild", priority = 1)]
         public static void ShowWindow()
         {
             GetWindow<AutomatedBuildEditor>().Show();
         }
 
-        [ShowInInspector]
-        public Dictionary<SceneAsset, bool> SceneSettings
-        {
-            get
-            {
-                if (buildSettings == null)
-                    return new Dictionary<SceneAsset, bool>();
-                _settingsDictionary = new Dictionary<SceneAsset, bool>();
-                _settingsDictionary = buildSettings.SceneList;
-                return _settingsDictionary;
-            }
-            set
-            {
-            }
-        }
-
-        [ShowInInspector]
-        public string Version
-        {
-            get{ return PlayerSettings.bundleVersion; }
-            set { PlayerSettings.bundleVersion = value; }
-        }
-
         [Button]
         private void Build()
         {
-            _buildNumber = 0;
-            _finishedBuilds = 0;
+            sceneList.Clear();
+            editorBuilds.Clear();
+            _buildCount = 0;
+            _currentBuild = 0;
 
-            for (int i = 0; i < _settingsDictionary.Count; i++)
+            if(SceneBuildType.BuildLeft && SceneBuildType.BuildRight && SceneBuildType.BuildCenter)
             {
-                if(_settingsDictionary.Values.ElementAt(i))
+                _buildCount = 3;
+            }
+            else if(atLeastTwo(SceneBuildType.BuildLeft, SceneBuildType.BuildRight, SceneBuildType.BuildCenter))
+            {
+                _buildCount = 2;
+            }
+            else if(atLeastOne(SceneBuildType.BuildLeft, SceneBuildType.BuildRight, SceneBuildType.BuildCenter))
+            {
+                _buildCount = 1;
+            }
+            else
+            {
+                _buildCount = 0;
+                Debug.LogError("Sorry no scene was chosen to make a build.");
+                return;
+            }
+
+
+            for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+            {
+               // Debug.Log(EditorBuildSettings.scenes[i].path);
+                //add to the list the scenes
+                sceneList.Add(AssetDatabase.LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[i].path));
+            }
+
+            for (int i = 0; i < sceneList.Count; i++)
+            {
+                if(sceneList[i].name.Contains("right") || sceneList[i].name.Contains("Right"))
                 {
-                    _buildNumber++;
+                    rightScene = sceneList[i];
+                    EditorBuildSettingsScene scene = new EditorBuildSettingsScene();
+                    scene.path = AssetDatabase.GetAssetPath(sceneList[i]);
+                    scene.enabled = false;
+                    editorBuilds.Add(scene);
+
                 }
-            }
-            Debug.Log("In total there will be " + _buildNumber + " builds.");
-            if(_buildNumber == 0)
-            {
-                Debug.Log("No scenes are selected. Can't make a build");
-                return;
-            }
-             MakeABuild();
-        }
-
-        private void SetupBuildSettings()
-        {
-            if (_buildNumber <= _finishedBuilds)
-                return;
-            List<EditorBuildSettingsScene> editorBuildSettingsScenes = new List<EditorBuildSettingsScene>();
-            SceneAsset currentScene = _settingsDictionary.Keys.ElementAt(_finishedBuilds);
-            string scenePath = AssetDatabase.GetAssetPath(currentScene);
-            if (!string.IsNullOrEmpty(scenePath))
-            {
-                EditorBuildSettingsScene item = new EditorBuildSettingsScene(scenePath, _settingsDictionary.Values.ElementAt(_finishedBuilds));
-                if (!editorBuildSettingsScenes.Contains(item))
+                else if (sceneList[i].name.Contains("left") || sceneList[i].name.Contains("Left"))
                 {
-                    editorBuildSettingsScenes.Add(item);
-                    if(currentScene.name.Contains("center") || currentScene.name.Contains("Center") || currentScene.name.Contains("Central"))
-                    {
-                        _buildType = BuildType.Center;
-                    }
-                    else if (currentScene.name.Contains("right") || currentScene.name.Contains("Right"))
-                    {
-                        _buildType = BuildType.Right;
-                    }
-                    else if (currentScene.name.Contains("left") || currentScene.name.Contains("Left"))
-                    {
-                        _buildType = BuildType.Left;
-                    }
+                    leftScene = sceneList[i];
+                    EditorBuildSettingsScene scene = new EditorBuildSettingsScene();
+                    scene.path = AssetDatabase.GetAssetPath(sceneList[i]);
+                    scene.enabled = false;
+                    editorBuilds.Add(scene);
+                }
+                else if (sceneList[i].name.Contains("center") || sceneList[i].name.Contains("Center"))
+                {
+                    centerScene = sceneList[i];
+                    EditorBuildSettingsScene scene = new EditorBuildSettingsScene();
+                    scene.path = AssetDatabase.GetAssetPath(sceneList[i]);
+                    scene.enabled = false;
+                    editorBuilds.Add(scene);
                 }
                 else
-                    return;
-            }
-            EditorBuildSettings.scenes = editorBuildSettingsScenes.ToArray();
-            editorBuildSettingsScenes.Clear();
-        }
-
-        private void MakeABuild()
-        {
-            SetupBuildSettings();
-            if (_buildType == BuildType.Center)
-            {
-                _currentName = PlayerSettings.productName + "_Center";
-            }
-            else if (_buildType == BuildType.Right)
-            {
-                _currentName = PlayerSettings.productName + "_Right";
-            }
-            else
-            {
-                _currentName = PlayerSettings.productName + "_Left";
-            }
-            string buildName = _currentName + @"\" +_currentName;
-            string buildPath;
-            if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.StandaloneWindows)
-                buildPath = Directory.GetCurrentDirectory() + @"\Builds\" + buildName + ".exe";
-            else
-                buildPath = Directory.GetCurrentDirectory() + @"\Builds\" + buildName;
-            Debug.Log("Build was made for " + _currentName);
-            _buildPath = Directory.GetCurrentDirectory() + @"\Builds\" + _currentName;
-            ChangeJsonFile();
-            BuildReport build = BuildPipeline.BuildPlayer(GetScenePaths(), buildPath, EditorUserBuildSettings.activeBuildTarget, BuildOptions.None);
-            if (build)
-            {
-                _finishedBuilds++;
-                Debug.Log("Finished making a build " + _finishedBuilds);
-                DeleteStreamingAssets();
-                
-                if (_buildNumber <= _finishedBuilds)
                 {
-                    Debug.Log("Made builds for all the scenes.Done");
-                    return;
+                    EditorBuildSettingsScene scene = new EditorBuildSettingsScene();
+                    scene.path = AssetDatabase.GetAssetPath(sceneList[i]);
+                    scene.enabled = true;
+                    editorBuilds.Add(scene);
                 }
-                MakeABuild();
             }
-            return;
+            DisableScenes();
+            MakeBuild();
         }
 
-        private void DeleteStreamingAssets()
+        private void MakeBuild()
         {
-            Debug.Log("<color=red>Deleting streaming assets</color>");
-            string filesToDelete;
-
-            if (_buildType == BuildType.Center)
-                filesToDelete = "center*";
-            else if (_buildType == BuildType.Right)
-                filesToDelete = "right*";
-            else
-                filesToDelete = "left*";
-
-            int x = 0;
-            string path = _buildPath + @"\" + _currentName + @"_Data\" + @"StreamingAssets\";
-            if (!Directory.Exists(path))
-                return;
-            string[] files = Directory.GetFiles(path, filesToDelete, SearchOption.AllDirectories);
-
-            foreach (string f in files)
+            //change the enabled shit
+            DisableScenes();
+            for (int i = 0; i < editorBuilds.Count; i++)
             {
-                File.Delete(f);
-                x++;
+                if (SceneBuildType.BuildLeft && (editorBuilds[i].path.Contains("left") || editorBuilds[i].path.Contains("Left")))
+                {
+                    editorBuilds[i].enabled = true;
+                    _buildType = BuildType.Left;
+                    SceneBuildType.BuildLeft = false;
+                    Debug.Log("Prepared to make a build for left screen " + editorBuilds[i].path);
+                    break;
+                }
+                else if (SceneBuildType.BuildRight && (editorBuilds[i].path.Contains("right") || editorBuilds[i].path.Contains("Right")))
+                {
+                    editorBuilds[i].enabled = true;
+                    _buildType = BuildType.Right;
+                    SceneBuildType.BuildRight = false;
+                    Debug.Log("Prepared to make a build for right screen " + editorBuilds[i].path);
+                    break;
+                }
+                else if (SceneBuildType.BuildCenter && (editorBuilds[i].path.Contains("center") || editorBuilds[i].path.Contains("Center")))
+                {
+                    editorBuilds[i].enabled = true;
+                    _buildType = BuildType.Center;
+                    SceneBuildType.BuildCenter = false;
+                    Debug.Log("Prepared to make a build for center screen " + editorBuilds[i].path);
+                    break;
+                }
+                else if(rightScene == null || leftScene == null || centerScene == null)
+                {
+                    if (SceneBuildType.BuildLeft)
+                    {
+                        _buildType = BuildType.Left;
+                        SceneBuildType.BuildLeft = false;
+                    }
+                    else if (SceneBuildType.BuildRight)
+                    {
+                        _buildType = BuildType.Right;
+                        SceneBuildType.BuildRight = false;
+                    }
+                    else if (SceneBuildType.BuildCenter)
+                    {
+                        _buildType = BuildType.Center;
+                        SceneBuildType.BuildCenter = false;
+                    }
+                    else
+                        return;
+                    break;
+                }
             }
-            Debug.Log("Deleted " + x + " files.");
+
+            _currentBuild++;
+            EditorBuildSettings.scenes = editorBuilds.ToArray();
+            List<string> scenesPaths = new List<string>();
+
+            _buildPath = Directory.GetCurrentDirectory() + "/Builds/" + PlayerSettings.productName + "_" + _buildType.ToString();
+            string buildPath = _buildPath + "/" + PlayerSettings.productName + "_" + _buildType.ToString() + ".exe";
+
+            for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+            {
+                if (EditorBuildSettings.scenes[i].enabled)
+                    scenesPaths.Add((EditorBuildSettings.scenes[i].path));
+            }
+            ChangeJsonFile();
+            BuildPlayerOptions buildOptions = new BuildPlayerOptions();
+            {
+                buildOptions.scenes = scenesPaths.ToArray();
+                buildOptions.locationPathName = buildPath;
+                buildOptions.target = BuildTarget.StandaloneWindows;
+                buildOptions.options = BuildOptions.None;
+            }
+            BuildReport report = BuildPipeline.BuildPlayer(buildOptions);
+            BuildSummary summary = report.summary;
+            if (summary.result == BuildResult.Failed)
+                return;
+            scenesPaths.Clear();
+            Debug.Log("Made a build for " + _buildType.ToString());
+            DeleteFiles();
+            if(_buildCount > _currentBuild)
+            {
+                Debug.Log("Will make a next build");
+                MakeBuild();
+            }
+            else
+            {
+                Debug.Log("<color=green>Finished making builds</color>");
+                return;
+            }
+        }
+
+        private void DisableScenes()
+        {
+            for (int i = 0; i < editorBuilds.Count; i++)
+            {
+                if (editorBuilds[i].path.Contains("left") || editorBuilds[i].path.Contains("Left"))
+                {
+                    editorBuilds[i].enabled = false;
+                }
+                else if (editorBuilds[i].path.Contains("right") || editorBuilds[i].path.Contains("Right"))
+                {
+                    editorBuilds[i].enabled = false;
+                }
+                else if (editorBuilds[i].path.Contains("center") || editorBuilds[i].path.Contains("Center"))
+                {
+                    editorBuilds[i].enabled = false;
+                }
+            }
+        }
+
+        private bool atLeastTwo(bool a, bool b, bool c)
+        {
+            return a ? (b || c) : (b && c);
+        }
+
+        private bool atLeastOne(bool a, bool b, bool c)
+        {
+            if (a && !b && !c)
+                return true;
+            else if (!a && b && !c)
+                return true;
+            else if (!a && !b && c)
+                return true;
+            else
+                return false;
         }
 
         private void ChangeJsonFile()
@@ -202,9 +270,42 @@ namespace Custom.Tool
                 saveData.screen = "Right";
             else
                 saveData.screen = "Left";
+
+
             string jsonInfo = JsonUtility.ToJson(saveData, true);
             File.WriteAllText(jsonFile,jsonInfo);
            
+        }
+
+        private void DeleteFiles()
+        {
+            Debug.Log("<color=red>Deleting files</color>");
+            if(_buildType == BuildType.Center)
+            {
+               var files =  Directory.GetFiles(_buildPath, "*center*");
+                for (int i = 0; i < files.Length; i++)
+                {
+                    Debug.Log(files[i]);
+                }
+            }
+           
+            else if (_buildType == BuildType.Right)
+            {
+                var files = Directory.GetFiles(_buildPath, "*right*");
+                for (int i = 0; i < files.Length; i++)
+                {
+                    Debug.Log(files[i]);
+                }
+            }
+            else if (_buildType == BuildType.Left)
+            {
+                var files = Directory.GetFiles(_buildPath, "*left*");
+                for (int i = 0; i < files.Length; i++)
+                {
+                    Debug.Log(files[i]);
+                }
+            }
+
         }
 
         private string[] GetScenePaths()
@@ -217,6 +318,13 @@ namespace Custom.Tool
             }
 
             return scenes;
+        }
+
+        private string GetProjectFolderPath()
+        {
+            var s = Application.dataPath;
+            s = s.Substring(s.Length - 7, 7); // remove "Assets/"
+            return s;
         }
     }
 }
